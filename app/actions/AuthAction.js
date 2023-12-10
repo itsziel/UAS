14c4bd90622ec6e6843b98da24ef179e1677cd65
hfizhirfan <hafizh.irfansyah41@gmail.com>
2023-12-11 00:08:02 +0700

    waduhh

---
+++ app/actions/AuthAction.js
@@ -0,0 +1,37 @@
+import { Alert } from "react-native";
+import FIREBASE from "../config/FIREBASE";
+import { clearStorage, getData, storeData } from "../utils/localStorage";
+
+
+export const loginUser = async (email, password) => {
+  try {
+    const success = await FIREBASE.auth().signInWithEmailAndPassword(email, password);
+    const resDB = await FIREBASE.database()
+      .ref("/users/" + success.user.uid)
+      .once("value");
+
+    if (resDB.val()) {
+      // Local storage (Async Storage)
+      await storeData("user", resDB.val());
+      return resDB.val();
+    } else {
+      throw new Error("User data not found");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const logoutUser = () => {
+  FIREBASE.auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+      clearStorage();
+    })
+    .catch((error) => {
+      // An error happened.
+      alert(error);
+    });
+};
+
