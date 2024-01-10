2745da8a955fa487f50941aa9b51b91fac01fe46
mhdzidnn <111571229+mhdzidnn@users.noreply.github.com>
2024-01-10 11:35:16 +0700

    Update detailsertif.js

--- app/detailsertif.js
+++ app/detailsertif.js
@@ -3,8 +3,16 @@ import { View, TouchableOpacity, Alert, Image } from "react-native";
 import * as FileSystem from 'expo-file-system';
 import * as MediaLibrary from 'expo-media-library';
 
+// -	Komponen ini menerima properti route dari navigasi. Properti ini mengandung parameter yang dikirimkan dari 
+//    komponen lain, seperti imageUri
 const Detailsertif = ({ route }) => {
   const { imageUri } = route.params;
+
+// -	Membuat objek downloadResumable menggunakan File System.createDownloadResumable dengan URL gambar yang akan diunduh (imageUri) dan path tempat penyimpanan lokal (FileSystem.documentDirectory + 'sertifikat.jpg').
+// -	Melakukan proses unduhan dengan downloadResumable. Download Async().
+// -	Meminta izin akses ke galeri foto perangkat menggunakan MediaLibrary.requestPermissionsAsync().
+// -	Jika izin diberikan (status === 'granted'), menyimpan gambar ke galeri foto perangkat menggunakan MediaLibrary. saveTo LibraryAsync(uri).
+// -	Memberikan umpan balik ke pengguna melalui Alert tergantung pada hasil operasi unduhan dan penyimpanan.
 
   const handleDownload = async () => {
     try {
